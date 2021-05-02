551ebe9104a0a1fd54cc2f2694531927fa69b1dd
Krishna Mahato <71918441+krishna9304@users.noreply.github.com>
2021-05-02 13:25:32 +0530

    Landing page

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div id="box1"></div>
+
+      <div id="box2">
+        <div id="text">
+          THE
+          <br />
+          COTERIE
+          <br />
+          CRUX
+        </div>
+      </div>
+
+      <div id="box3">
+        <div id="container">
+          <div id="logo">
+            <h2>LOGO</h2>
+          </div>
+          <div id="menu">
+            <ul>
+              <li>HOME</li>
+              <li>SERVICE</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+              <li>HELP</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
