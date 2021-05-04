14cfb71b1221dbd80a6ae6440791c3741850cbf6
Mind0Bender <mrcircuit1234@gmail.com>
2021-05-04 16:23:07 +0530

    Circle Orbitor done

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,141 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
-import background1 from "./images/1.jpg"
-import background2 from "./images/2.jpg"
-import background3 from "./images/3.jpg"
-import background4 from "./images/4.jpg"
-import background5 from "./images/5.jpg"
+import background1 from "./images/1.jpg";
+import background2 from "./images/2.jpg";
+import background3 from "./images/3.jpg";
+import background4 from "./images/4.jpg";
+import background5 from "./images/5.jpg";
 
 export default function App() {
-  let [offset , setoffset] = useState(1256);
-  let [background , setbackground] = useState(background1);
+  let [angle, setAngle] = useState(-10);
+  let [offset, setoffset] = useState(1256);
+  let [background, setbackground] = useState(background1);
+  let degrees_to_radians = (degrees) => {
+    var pi = Math.PI;
+    return degrees * (pi / 180);
+  };
+
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAngle((angle) => angle - 0.1);
+      console.log("lola", seconds);
+    }, 1000 / 60);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App" style={{backgroundImage : `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${background})` , transition: "600ms"}}>
+    <div
+      className="App"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${background})`,
+        transition: "600ms",
+      }}
+    >
       <div id="box1">
-        <svg height = "402" width = "402">
-          <circle r="200" cx="201" cy="201" stroke-width="1px" stroke="grey" fill="none" />
+        <svg height="400" width="400">
+          <circle
+            r="200"
+            cx="201"
+            cy="201"
+            strokeWidth="1px"
+            stroke="grey"
+            fill="none"
+          />
         </svg>
-        <svg height = "402" width = "402">
-          <circle r="200" cx="201" cy="201" stroke-width="1px" stroke="#fff" fill="none" style={{strokeDasharray: "1256", 
-          strokeDashoffset: `${offset}`,
-          transition: "1s"}} />
+        <svg height="402" width="402">
+          <circle
+            r="200"
+            cx="201"
+            cy="201"
+            strokeWidth="1px"
+            stroke="#fff"
+            fill="none"
+            style={{
+              strokeDasharray: "1256",
+              strokeDashoffset: `${offset}`,
+              transition: "1s",
+            }}
+          />
         </svg>
-        <div id="circleBox">
-          <span onClick={() => {if (offset===251) {
-            setoffset(0);
-            setbackground(background1);
-          } 
-          else if(offset===0) {
-            setoffset(1256);
-            setbackground(background1);
-          }else{
-            setbackground(background1);
-          }}} id="step1"></span>
-          <span onClick={() => {
-            setoffset(1006);
-            setbackground(background2)
-            }} id="step2"></span>
-          <span onClick={() =>{
-            setoffset(754);
-            setbackground(background3)
-             }} id="step3"></span>
-          <span onClick={() =>{
-            setoffset(502);
-            setbackground(background4)
-             }} id="step4"></span>
-          <span onClick={() =>{
-            setoffset(251);
-            setbackground(background5)
-             }} id="step5"></span>
+        <div id="circleBox" z>
+          <span
+            onClick={() => {
+              if (offset === 251) {
+                setoffset(0);
+                setbackground(background1);
+              } else if (offset === 0) {
+                setoffset(1256);
+                setbackground(background1);
+              } else {
+                setbackground(background1);
+              }
+            }}
+            style={{
+              left: `${200 * Math.sin(degrees_to_radians(angle)) - 23}px`,
+              bottom: `${200 * Math.cos(degrees_to_radians(angle)) + 80}px`,
+            }}
+            id="step1"
+          ></span>
+          <span
+            onClick={() => {
+              setoffset(1006);
+              setbackground(background2);
+            }}
+            style={{
+              left: `${200 * Math.sin(degrees_to_radians(angle + 72)) - 23}px`,
+              bottom: `${
+                200 * Math.cos(degrees_to_radians(angle + 72)) + 80
+              }px`,
+            }}
+            id="step2"
+          ></span>
+          <span
+            onClick={() => {
+              setoffset(754);
+              setbackground(background3);
+            }}
+            style={{
+              left: `${
+                200 * Math.sin(degrees_to_radians(angle + 2 * 72)) - 23
+              }px`,
+              bottom: `${
+                200 * Math.cos(degrees_to_radians(angle + 2 * 72)) + 80
+              }px`,
+            }}
+            id="step3"
+          ></span>
+          <span
+            onClick={() => {
+              setoffset(502);
+              setbackground(background4);
+            }}
+            style={{
+              left: `${
+                200 * Math.sin(degrees_to_radians(angle + 3 * 72)) - 23
+              }px`,
+              bottom: `${
+                200 * Math.cos(degrees_to_radians(angle + 3 * 72)) + 80
+              }px`,
+            }}
+            id="step4"
+          ></span>
+          <span
+            onClick={() => {
+              setoffset(251);
+              setbackground(background5);
+            }}
+            style={{
+              left: `${
+                200 * Math.sin(degrees_to_radians(angle + 4 * 72)) - 23
+              }px`,
+              bottom: `${
+                200 * Math.cos(degrees_to_radians(angle + 4 * 72)) + 80
+              }px`,
+            }}
+            id="step5"
+          ></span>
         </div>
       </div>
 
