31792ec81a481d2f693a0036fa6e485ec303f266
Krishna Mahato <71918441+krishna9304@users.noreply.github.com>
2021-05-06 18:35:27 +0530

    page transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
-import react from "react";
+import React from "react";
+import "./styles.css"
 import Homepage from "./pages/homepage";
-import { BrowserRouter } from "react-router-dom";
-import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import Helppage from "./pages/helppage";
 import Servicepage from "./pages/servicepage"
 import Aboutpage from "./pages/aboutpage"
 import Contactpage from "./pages/contactpage"
+import { Route, Switch} from "react-router-dom";
+import { CSSTransition , TransitionGroup } from "react-transition-group";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          <Homepage />
-        </Route>
-        <Route path="/help">
-          <Helppage />
-        </Route>
-        <Route path="/about">
-          <Aboutpage />
-        </Route>
-        <Route path="/service">
-          <Servicepage />
-        </Route>
-        <Route path="/contact">
-          <Contactpage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Route render={({location})=>(
+        <TransitionGroup>
+          <CSSTransition
+            key={location.key}
+            timeout={500}
+            classNames="fade"
+          >
+            <Switch location={location} >
+            <Route path="/" exact>
+              <Homepage />
+            </Route>
+            <Route path="/help" exact>
+              <Helppage />
+            </Route>
+            <Route path="/about" exact>
+              <Aboutpage />
+            </Route>
+            <Route path="/service" exact>
+              <Servicepage />
+            </Route>
+            <Route path="/contact" exact>
+              <Contactpage />
+            </Route>
+          </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )} />
+    </>
   )
 }
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,20 +1,11 @@
 import react from "react";
 import "./index.css"
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import { useState } from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 const Navbar = () => {
-    let [classlist, setclass] = useState("");
-    let add = (e , loc)=>{
-        e.preventDefault();
-        setclass("slide");
-        setTimeout(() => {
-            window.location = loc;
-        }, 900);
-    }
     return(
-        <div>
-            <div className= {`transition ${classlist}`} >
+        <div className="box3" >
+            <div className= {"transition"} >
                 <div className="cover cover1"></div>
                 <div className="cover cover2"></div>
                 <div className="cover cover3"></div>
@@ -25,21 +16,11 @@ const Navbar = () => {
             </div>
             <div id="menu">
                 <ul>
-                <Link to="/" onClick={(e)=>{
-                    add(e , "/");
-                }} style={{ color: 'rgba(255, 255, 255, 0.644)'}} ><li>HOME</li></Link>
-                <Link onClick={(e)=>{
-                    add(e , "/service");
-                }} to="/service" style={{ color: 'rgba(255, 255, 255, 0.644)' }} ><li>SERVICE</li></Link>
-                <Link onClick={(e)=>{
-                    add(e , "/about");
-                }} to="/about" style={{ color: 'rgba(255, 255, 255, 0.644)' }} ><li>ABOUT</li></Link>
-                <Link onClick={(e)=>{
-                    add(e , "/contact");
-                }} to="/contact" style={{ color: 'rgba(255, 255, 255, 0.644)' }} ><li>CONTACT</li></Link>
-                <Link onClick={(e)=>{
-                    add(e , "/help");
-                }} to="/help" style={{ color: 'rgba(255, 255, 255, 0.644)' }} ><li>HELP</li></Link>
+                <NavLink className="nav-link-gdc" activeClassName="nav-link-gdc-active" to="/" ><li>HOME</li></NavLink>
+                <NavLink className="nav-link-gdc" activeClassName="nav-link-gdc-active" to="/service" ><li>SERVICE</li></NavLink>
+                <NavLink className="nav-link-gdc" activeClassName="nav-link-gdc-active" to="/about" ><li>ABOUT</li></NavLink>
+                <NavLink className="nav-link-gdc" activeClassName="nav-link-gdc-active" to="/contact" ><li>CONTACT</li></NavLink>
+                <NavLink className="nav-link-gdc" activeClassName="nav-link-gdc-active" to="/help" ><li>HELP</li></NavLink>
                 </ul>
             </div>
             </div>
